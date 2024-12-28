12ea38a7d983e5f82597a8b1201a650ea7fc9503
refleks1a <fhbxdfh.dhncdtb@gmail.com>
2024-12-28 12:53:50 +0400

    upload files to aws s3

--- API/main.py
+++ API/main.py
@@ -7,6 +7,8 @@ import os
 from dotenv import load_dotenv
 
 from routers import auth, venues, events, social_auth, chat_rooms
+from aws import s3
+
 
 # Create FastAPI application
 app = FastAPI()
@@ -38,3 +40,4 @@ app.include_router(venues.router)
 app.include_router(events.router)
 app.include_router(social_auth.router)
 app.include_router(chat_rooms.router)
+app.include_router(s3.router)
